6031c2d6c5c7662db03406af4bf6fdc31b7a240f
Stefano J. Attardi <stefano.attardi@coinbase.com>
2019-09-15 09:46:16 -0700

    Update Webpack config

--- etc/webpack.config.js
+++ etc/webpack.config.js
@@ -8,38 +8,52 @@ var debug = process.env.NODE_ENV != 'production';
 var profile = process.env.PROFILE == 'true';
 
 module.exports = {
-  entry: debug ? [
-    'webpack-hot-middleware/client?path=' + config.ASSETS_URL + '/__webpack_hmr',
-    './src/captured',
-  ] : [
-    './src/captured',
-  ],
+  entry: debug
+    ? [
+        'webpack-hot-middleware/client?path=' +
+          config.ASSETS_URL +
+          '/__webpack_hmr',
+        './src/captured'
+      ]
+    : ['./src/captured'],
   module: {
-    loaders: [
-      {test: /\.js$/, loader: 'babel', exclude: /node_modules/},
-      {test: /\.json$/, loader: 'json-loader'},
-    ],
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: { transpileOnly: true }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     library: 'Captured',
     filename: debug ? 'captured.js' : 'captured-[hash].js',
     path: path.join(__dirname, '..', config.ASSETS_DIR + '/'),
     publicPath: config.ASSETS_URL + '/',
-    pathinfo: debug && !profile,
+    pathinfo: debug && !profile
   },
   externals: {
-    StripeCheckout: 'var StripeCheckout',
+    StripeCheckout: 'var StripeCheckout'
   },
-  devtool: debug && ['eval', 'inline-source-map'],
+  devtool: debug && 'inline-source-map',
   plugins: [
-    !debug && new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
-    !profile && !debug && new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      comments: false,
-      compress: {warnings: false},
-    }),
-    !debug && new AssetsPlugin({path: './assets', filename: 'manifest.json'}),
+    !debug &&
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+    !profile &&
+      !debug &&
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: false,
+        comments: false,
+        compress: { warnings: false }
+      }),
+    !debug && new AssetsPlugin({ path: './assets', filename: 'manifest.json' }),
     debug && new webpack.HotModuleReplacementPlugin(),
-    !debug && new webpack.optimize.DedupePlugin(),
-  ].filter(function(x) { return !!x }),
+    !debug && new webpack.optimize.DedupePlugin()
+  ].filter(function(x) {
+    return !!x;
+  })
 };
