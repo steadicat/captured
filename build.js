6450e98d21a5a9fd51ea9f0329194c73e78386c3
Stefano J. Attardi <stefano.attardi@coinbase.com>
2019-09-15 09:52:08 -0700

    Clean up config files

--- build.js
+++ build.js
@@ -1,34 +1,36 @@
-/* eslint-env node */
-/* eslint no-console: 0, no-var: 0 */
-require('babel-core/register')();
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
-var ducts = require('ducts');
-var ReactDOMServer = require('react-dom/server');
-var Page = require('./src/ui/page');
-var actions = require('./src/actions');
-var data = require('./src/data');
+import * as Page from './src/ui/page';
+import * as ReactDOMServer from 'react-dom/server';
+import * as fs from 'fs';
+import * as mkdirp from 'mkdirp';
+import * as path from 'path';
+
+import data from './src/data';
+import {initialState} from './src/appState';
 
 var paths = {
-  '/': 'assets/index.html',
-  '/about': 'assets/about/index.html',
-  '/orders': 'assets/orders/index.html',
-  '/notfound': 'assets/notfound.html',
-  '/error': 'assets/error.html',
+  '/': 'app/assets/index.html'
+  /*  '/about': 'app/assets/about/index.html',
+  '/orders': 'app/assets/orders/index.html',
+  '/notfound': 'app/assets/notfound.html',
+  '/error': 'app/assets/error.html'*/
 };
-
+/*
 for (var i = 0, l = data.length; i < l; i++) {
-  paths['/' + data[i].id] = 'assets/' + data[i].id + '/index.html';
-  paths['/' + data[i].id + '/references'] = 'assets/' + data[i].id + '/references/index.html';
+  paths['/' + data[i].id] = 'app/assets/' + data[i].id + '/index.html';
+  paths['/' + data[i].id + '/references'] =
+    'app/assets/' + data[i].id + '/references/index.html';
   if (data[i].artistContact) {
-    paths['/' + data[i].id + '/contact'] = 'assets/' + data[i].id + '/contact/index.html';
+    paths['/' + data[i].id + '/contact'] =
+      'app/assets/' + data[i].id + '/contact/index.html';
   }
 }
+*/
 
 for (var p in paths) {
   mkdirp.sync(path.dirname(paths[p]));
-  var store = actions.init(p);
-  var state = ducts.createStore(store, actions);
-  fs.writeFileSync(paths[p], '<!DOCTYPE html>' + ReactDOMServer.renderToString(Page.createPage(state.get, state.actions)));
+  fs.writeFileSync(
+    paths[p],
+    '<!DOCTYPE html>' +
+      ReactDOMServer.renderToString(Page.createPage({...initialState, path: p}))
+  );
 }
--- buildconfig.js
+++ buildconfig.js
@@ -3,33 +3,48 @@
 var fs = require('fs');
 var findit = require('findit2');
 
-const FILES = 'assets';
-const TEMPLATE = 'app.template.yaml';
-const OUTPUT = 'app.yaml';
+const ROOT = 'app/';
+const FILES = 'app/assets';
+const TEMPLATE = 'app/app.template.yaml';
+const OUTPUT = 'app/app.yaml';
 const PLACEHOLDER = '%STATIC%\n';
 
 function handler(url, file, add) {
-  //url = url.replace(/^assets/, '');
+  // url = url.replace(/^app/, '');
   if (/\.html$/.test(file)) {
     url = url.replace(/\/index\.html$/, '');
     url = url.replace(/\.html$/, '');
 
     if (/^\/orders(\/|$)/.test(url)) {
-      add({url: url + '/?', file: file, mime_type: 'text/html; charset=UTF-8', login: 'admin'});
+      add({
+        url: url + '/?',
+        file: file,
+        mime_type: 'text/html; charset=UTF-8',
+        login: 'admin'
+      });
     } else {
-      add({url: url + '/?', file: file, mime_type: 'text/html; charset=UTF-8'});
+      add({
+        url: url + '/?',
+        file: file,
+        mime_type: 'text/html; charset=UTF-8'
+      });
     }
   } else if (/\.js(on)?$/.test(file)) {
     // skip
   } else {
     // skip
   }
-};
+}
 
 function toYaml(obj) {
-  return '- ' + Object.keys(obj).map(key => {
-    return key + ': ' + obj[key];
-  }).join('\n  ');
+  return (
+    '- ' +
+    Object.keys(obj)
+      .map(key => {
+        return key + ': ' + obj[key];
+      })
+      .join('\n  ')
+  );
 }
 function generate(yaml) {
   var entries = '';
@@ -43,27 +58,27 @@ function generate(yaml) {
     info.url = info.url.replace(/\./g, '\\.');
     info.secure = 'always';
     var entry = toYaml(info);
-    //console.log(entry);
+    // console.log(entry);
     entries += entry + '\n';
   }
 
   var finder = findit(FILES);
 
-  finder.on('file', function (file) {
+  finder.on('file', function(file) {
     var request = file.substring(FILES.length);
+    file = file.substring(ROOT.length);
     if (/\/\./.test(file)) return;
     return handler(request, file, addEntry);
   });
 
-  finder.on('end', function () {
+  finder.on('end', function() {
     yaml = yaml.replace(PLACEHOLDER, entries);
 
-    fs.writeFile(OUTPUT, yaml, function (error) {
+    fs.writeFile(OUTPUT, yaml, function(error) {
       if (error) return console.error(error);
     });
   });
 }
-
 
 fs.readFile(TEMPLATE, 'utf8', function(error, data) {
   if (error) return console.error(error);
